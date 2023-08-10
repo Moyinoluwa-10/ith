21351fc5f9ba2078119d6ffeb252c04906c7ee07
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-08-10 06:31:50 -0500

    feat: tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./about/**/*.{html,js}", "./team/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
