a2aadbc189e66957887ae3e29ac009f1a22e5029
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-08-18 09:14:44 -0500

    feat: file restructure config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./about/**/*.{html,js}",
+    "./contact/**/*.{html,js}",
+    "./donate/**/*.{html,js}",
+  ],
   theme: {
     fontFamily: {
       u8: ["U8-Bold", "sans-serif"],
